7081ef4ad307726bd9c7aefb99d9ab4b2e25dfae
tcaro <troycaro@gmail.com>
2012-11-02 17:36:27 -0400

    still working on this

---
+++ treeMap/treeMap.py
@@ -0,0 +1,127 @@
+#!/usr/local/bin/python3
+
+"""
+Title: treeMap.py
+Language: python3
+Assignment: Homework 8
+Author: Troy Caro <twc9438@rit.edu>
+Date: November 1, 2012
+Purpose: Create a map structure based on a binary tree.
+"""
+
+######################################################################################################
+# MAP CLASS DEFINITIONS
+######################################################################################################
+
+
+class EmptyMap():
+    """
+    Makes an empty map instance.
+    """
+    __slots__ = ()
+
+
+class NonEmptyMap():
+    """
+    Makes a map instance with values left, key, value, and right
+    """
+    __slots__ = ('left', 'key', 'value', 'right')
+
+######################################################################################################
+# MAP CONSTRUCTOR FUNCTIONS
+######################################################################################################
+
+
+def mkEmptyMap():
+    """
+    Makes an empty map
+
+    Post-conditions:
+        Returns an EmptyMap instance
+    """
+    return EmptyMap()
+
+
+def mkNonEmptyMap(left, key, value, right):
+    """
+    Makes a NonEmptyMap instance with data.
+
+    Parameters:
+        left (map) - left map object
+        key (string) - the key value in the map object
+        value (int) - the value associated with the key
+        right (map) - right map object
+    Pre-conditions:
+        left and right are either of type EmptyMap or NonEmptyMap
+    Post-conditions:
+        Returns and instance of a NonEmptyMap
+    """
+    m = NonEmptyMap()
+    m.left = left
+    m.key = key
+    m.value = value
+    m.right = right
+    return m
+
+######################################################################################################
+# MAP FUNCTION DEFINITIONS
+######################################################################################################
+
+
+def mapInsert(key, value, map):
+    """
+    Inserts a new key and value into a map.
+
+    Parameters:
+        key (string) - the key value in the map
+        value (int) - the value associated with the key
+        map (NonEmptyMap) - an instance of NonEmptyMap
+    Pre-conditions:
+        map is an instance of NonEmptyMap
+    Post-conditions:
+        Returns an instance of NonEmptyMap with the new key and value added
+    """
+    if (key > map.value):
+        mapInsert(key, value, map.left)
+        return mkNonEmptyMap(mkEmptyMap(), key, value, mkEmptyMap())
+    elif (key < map.value):
+        mapInsert(key, value, map.right)
+        return mkNonEmptyMap(mkEmptyMap(), key, value, mkEmptyMap())
+    else:
+        map.value = value
+
+
+def mapToString(map):
+    """
+    Makes a string that represents a map.
+
+    Parameters:
+        map (NonEmptyMap) - instance of NonEmptyMap
+    Pre-conditions:
+        map is an instance of NonEmptyMap
+    Post-conditions:
+        Returns a string that represents a map
+    """
+    pass
+
+
+def mapSearch(key, map):
+    """
+    Searches a map for a key and gets the value associated with it.
+
+    Parameters:
+        key (string) - the key value in the map
+        map (NonEmptyMap) - instance of NonEmptyMap
+    Pre-conditions:
+        map is an instance of NonEmptyMap
+    Post-conditions:
+        Returns the value associated with the key, or None if the key is not there"""
+    pass
+
+######################################################################################################
+# MAIN FUNCTION DEFINITION
+######################################################################################################
+
+
+def main():
+    pass
